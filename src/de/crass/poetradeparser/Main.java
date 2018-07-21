b97ee135331a31a5defacdd4a2fe3fbd7893b6dd
MassCraxx <masscrass@users.noreply.github.com>
2018-07-21 23:49:10 +0200

    version 0.2.1

--- src/de/crass/poetradeparser/Main.java
+++ src/de/crass/poetradeparser/Main.java
@@ -25,7 +25,7 @@ import java.util.List;
 
 public class Main extends Application implements ParseListener {
 
-    public static final String versionText = "v0.2b";
+    public static final String versionText = "v0.2.1";
 
     @FXML
     private ListView<CurrencyDeal> playerDealList;
