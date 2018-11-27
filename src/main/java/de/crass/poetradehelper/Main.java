84fb86f3395245b848ff5e880aef26abbc8f65bb
MassCraxx <masscrass@users.noreply.github.com>
2018-11-27 23:28:43 +0100

    version 0.4.3

--- src/main/java/de/crass/poetradehelper/Main.java
+++ src/main/java/de/crass/poetradehelper/Main.java
@@ -46,7 +46,7 @@ import java.util.concurrent.*;
 public class Main extends Application implements ParseListener {
 
     public static final String title = "PoeTradeHelper";
-    public static final String versionText = "v0.4.3-SNAPSHOT";
+    public static final String versionText = "v0.4.3";
 
     @FXML
     private ListView<CurrencyDeal> playerDealList;
